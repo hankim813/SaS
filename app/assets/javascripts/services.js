d0ec56c2cf0bf1be6bf9640878e9dcf92481983d
Han Kim <hankim813@gmail.com>
2014-09-22 14:40:55 -0500

    ready for mobile optimization

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,27 +1,52 @@
 $(document).ready(function() {
   $("#corporate").click(function() {
-    $("#1").show();
-    $("#2").addClass("hide");
-    $("#3").addClass("hide");
+    $("#sec-corp").show();
+    $("#sec-business").addClass("hide");
+    $("#sec-corp").removeClass("hide");
+    $("#sec-startup").addClass("hide");
     $("#1a").addClass("active");
     $("#2a").removeClass("active");
     $("#3a").removeClass("active");
   });
   $("#business").click(function() {
-    $("#2").removeClass("hide");
-    $("#1").hide();
-    $("#3").addClass("hide");
+    $("#sec-business").removeClass("hide");
+    $("#sec-corp").hide();
+    $("#sec-startup").addClass("hide");
     $("#2a").addClass("active");
     $("#1a").removeClass("active");
     $("#3a").removeClass("active");
   });
   $("#startup").click(function() {
-    $("#3").removeClass("hide");
-    $("#1").hide();
-    $("#2").addClass("hide");
+    $("#sec-startup").removeClass("hide");
+    $("#sec-corp").hide();
+    $("#sec-business").addClass("hide");
     $("#3a").addClass("active");
     $("#1a").removeClass("active");
     $("#2a").removeClass("active");
-  })
+  });
+  $("#1a").click(function() {
+    $("#1a").addClass("active");
+    $("#2a").removeClass("active");
+    $("#3a").removeClass("active");
+    $("#sec-corp").removeClass("hide");
+    $("#sec-business").addClass("hide");
+    $("#sec-startup").addClass("hide");
+  });
+  $("#2a").click(function() {
+    $("#2a").addClass("active");
+    $("#1a").removeClass("active");
+    $("#3a").removeClass("active");
+    $("#sec-business").removeClass("hide");
+    $("#sec-corp").addClass("hide");
+    $("#sec-startup").addClass("hide");
+  });
+  $("#3a").click(function() {
+    $("#3a").addClass("active");
+    $("#2a").removeClass("active");
+    $("#1a").removeClass("active");
+    $("#sec-startup").removeClass("hide");
+    $("#sec-business").addClass("hide");
+    $("#sec-corp").addClass("hide");
+  });
 });
 
