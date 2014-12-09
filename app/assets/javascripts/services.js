05d08b7b55019d555d789a6a3266b4cb632d5927
Han Kim <hankim813@gmail.com>
2014-12-08 20:40:40 -0800

    css updates

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     $("#sec-business").addClass("hide");
     $("#sec-corp").removeClass("hide");
     $("#sec-startup").addClass("hide");
-    $("#1a").addClass("active");
-    $("#2a").removeClass("active");
-    $("#3a").removeClass("active");
+    $("#1a a").addClass("active");
+    $("#2a a").removeClass("active");
+    $("#3a a").removeClass("active");
     $("#corporate").addClass("hovered");
     $("#business").removeClass("hovered");
     $("#startup").removeClass("hovered");
@@ -15,9 +15,9 @@ $(document).ready(function() {
     $("#sec-business").removeClass("hide");
     $("#sec-corp").hide();
     $("#sec-startup").addClass("hide");
-    $("#2a").addClass("active");
-    $("#1a").removeClass("active");
-    $("#3a").removeClass("active");
+    $("#2a a").addClass("active");
+    $("#1a a").removeClass("active");
+    $("#3a a").removeClass("active");
     $("#corporate").removeClass("hovered");
     $("#business").addClass("hovered");
     $("#startup").removeClass("hovered");
@@ -26,9 +26,9 @@ $(document).ready(function() {
     $("#sec-startup").removeClass("hide");
     $("#sec-corp").hide();
     $("#sec-business").addClass("hide");
-    $("#3a").addClass("active");
-    $("#1a").removeClass("active");
-    $("#2a").removeClass("active");
+    $("#3a a").addClass("active");
+    $("#1a a").removeClass("active");
+    $("#2a a").removeClass("active");
     $("#corporate").removeClass("hovered");
     $("#business").removeClass("hovered");
     $("#startup").addClass("hovered");
