42b3099a2817128166ca64db9f92c9742ff7273f
Han Kim <hankim813@gmail.com>
2014-12-08 20:50:35 -0800

    link fix

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -33,5 +33,39 @@ $(document).ready(function() {
     $("#business").removeClass("hovered");
     $("#startup").addClass("hovered");
   });
+  $('#1a a').click(function() {
+    $("#sec-corp").show();
+    $("#sec-business").addClass("hide");
+    $("#sec-corp").removeClass("hide");
+    $("#sec-startup").addClass("hide");
+    $("#1a a").addClass("active");
+    $("#2a a").removeClass("active");
+    $("#3a a").removeClass("active");
+    $("#corporate").addClass("hovered");
+    $("#business").removeClass("hovered");
+    $("#startup").removeClass("hovered");
+});
+  $('#2a a').click(function() {
+    $("#sec-business").removeClass("hide");
+    $("#sec-corp").hide();
+    $("#sec-startup").addClass("hide");
+    $("#2a a").addClass("active");
+    $("#1a a").removeClass("active");
+    $("#3a a").removeClass("active");
+    $("#corporate").removeClass("hovered");
+    $("#business").addClass("hovered");
+    $("#startup").removeClass("hovered");
+  });
+  $('#3a a').click(function() {
+    $("#sec-startup").removeClass("hide");
+    $("#sec-corp").hide();
+    $("#sec-business").addClass("hide");
+    $("#3a a").addClass("active");
+    $("#1a a").removeClass("active");
+    $("#2a a").removeClass("active");
+    $("#corporate").removeClass("hovered");
+    $("#business").removeClass("hovered");
+    $("#startup").addClass("hovered");
+  });
 });
 
